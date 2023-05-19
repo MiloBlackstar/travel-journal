1135e45d04d5812cb6504ae58d91fec18600d871
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-19 17:46:39 +0200

    Add navbar and setup basic style rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+import Data from "./Data"
+import Navbar from "./compounds/navbar"
 
+export default function App() {
+  return (
+    <div className="page">
+      <Navbar />
+    </div> 
+  )
+}
---
+++ src/compounds/navbar.jsx
@@ -0,0 +1,10 @@
+import Logo from "../images/planet-logo.png"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={Logo}/>
+      <p>my travel journal.</p>
+    </nav>
+  )
+}
