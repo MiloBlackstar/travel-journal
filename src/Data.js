f1e15a6490bc88dde0108ecd10b124a21688f783
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-19 19:23:17 +0200

    Finish travel journal project

--- src/Data.js
+++ src/Data.js
@@ -8,5 +8,23 @@ export default [
       endDate: "24 Jan, 2021",
       description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
       imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  }, {
+    id: 1,
+      title: "Sydney Opera",
+      location: "AUSTRALIA",
+      googleMapsUrl: "https://www.google.com/maps/contrib/104638486623707479945/photos/@-33.8567844,151.2152967,17z/data=!3m1!4b1!4m3!8m2!3m1!1e1",
+      startDate: "3 Mar, 2022",
+      endDate: "7 Mar, 2022",
+      description: "The Sydney Opera House is one of the world's top tourist attractions and most recognizable buildings. This Australian landmark in Sydney is also known as one of the busiest performing arts venues in the world.",
+      imageUrl: "https://images.unsplash.com/photo-1595740229246-cfdda61917c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80"
+  }, {
+    id: 1,
+      title: "Machu Picchu",
+      location: "PERU",
+      googleMapsUrl: "https://www.google.com/maps/d/viewer?gl=US&ie=UTF8&oe=UTF8&msa=0&showlabs=1&mid=1C4E2c4tXNwMcXzJMvkv4_qEWnus&ll=-13.160910556682353%2C-72.533541&z=15",
+      startDate: "30 May, 2023",
+      endDate: "23 May, 2023",
+      description: "The ancient Incan citadel of Machu Picchu sits perched on a mountaintop in the Peruvian Andes at an altitude of nearly 8,000 feet above sea level.",
+      imageUrl: "https://images.unsplash.com/photo-1609944433409-81bda5323abc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
   }
 ]
